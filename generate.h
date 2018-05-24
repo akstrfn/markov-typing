a999b22d0e33e122ef7de3f9a74ed0fa63daa60a
Aleksandar Trifunovic <akstrfn@gmail.com>
2018-05-24 21:34:00 +0200

    add more characters to generate function

--- generate.h
+++ generate.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include <string>
 
-static char choice(const std::string& sequence){
+char choice(const std::string& sequence){
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(0, sequence.length() - 1);
@@ -15,10 +15,13 @@ static char choice(const std::string& sequence){
 std::string generate(int max_size){
     std::string out = "";
     std::string lowercase = "qwertyuiopasdfghjklzxcvbnm";
+    std::string uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    std::string symbols = "`~!@#$%^&*()-_=+{[]};:'\"\\|,<.>/?";
+    std::string numbers = "0123456789";
 
     for (int i = 0; i != max_size; ++i){
         if ((i + 1) % 5 != 0)
-            out.push_back(choice(lowercase));
+            out.push_back(choice(lowercase + symbols + uppercase + numbers));
         else
             out.push_back(' ');
     }
