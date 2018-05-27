9027abdba67562d0743d580743a0898fa564da5c
Aleksandar Trifunovic <akstrfn@gmail.com>
2018-05-27 21:33:24 +0200

    add function to stats that reports if the last character was correctly typed

--- stats.hh
+++ stats.hh
@@ -5,11 +5,23 @@
 #include <numeric>
 
 // TODO check for null characters
-auto missed_characters(const std::string& buffer,
+int missed_characters(const std::string& buffer,
                        const std::string& full_sentence){
+    if (buffer.length() == 0) return 0;
     return buffer.length() - std::inner_product(buffer.begin(), buffer.end(),
                               full_sentence.begin(), 0,
                               std::plus<>(), std::equal_to<>());
+}
+
+bool last_char_correct(const std::string& buffer,
+                      const std::string& full_sentence){
+    if (buffer.length() == 0)
+        throw;
+
+    int pos = buffer.length() - 1;
+    if (buffer[pos] == full_sentence[pos])
+        return true;
+    return false;
 }
 
 #endif /* ifndef STATS_H */
